514adca96b860bd509bae7baf5513fa6dbd03819
web-cook <94051082+web-cook@users.noreply.github.com>
2021-11-10 15:26:46 +0300

    Lemon

---
+++ js/script.js
@@ -0,0 +1,63 @@
+"use strict"
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows()
+        );
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+} else {
+    document.body.classList.add('_pc');
+}
+
+//Меню бургер
+
+const iconMenu = document.querySelector('.menu_icon');
+const menuBody = document.querySelector('.menu_body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+const menuBodyActive = document.querySelector('.menu_body._active')
+
+if (isMobile.any()){
+    let arrow = document.querySelectorAll('.menu_arrow');
+    for(let i = 0; i < arrow.length; i++){
+        let thisLink = arrow[i].previousElementSibling;
+        let subMenu = arrow[i].nextElementSibling;
+        let thisArrow = arrow[i];
+
+        thisLink.classList.add('parent');
+        arrow[i].addEventListener('click', function(){
+            subMenu.classList.toggle('open');
+            thisArrow.classList.toggle('active');
+        })
+    }
+}
